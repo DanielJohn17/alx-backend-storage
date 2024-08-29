d467cd44aaa3ea27b14d74f5fcf314e307ba5c37
DanielJohn17 <danielyohannes38@gmail.com>
2024-08-29 16:36:23 +0300

    fixed documentation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
+'''Module for caching data in Redis'''
 import redis
 import uuid
 from typing import Union
 
 
 class Cache:
+    '''Class for caching data in Redis'''
+
     def __init__(self) -> None:
+        '''Initializes the Cache class'''
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''Stores data in Redis and returns a key'''
         key = str(uuid.uuid4())
         self._redis.set(key, data)
 
         return key
-
