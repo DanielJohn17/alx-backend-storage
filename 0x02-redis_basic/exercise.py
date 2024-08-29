dc023b833d83e958013e6342ecabf8d51d07a1a8
DanielJohn17 <danielyohannes38@gmail.com>
2024-08-29 18:30:31 +0300

    finished task 4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -37,6 +37,32 @@ def call_history(method: Callable) -> Callable:
     return invoker
 
 
+def replay(fn: Callable) -> None:
+    if fn is None or not hasattr(fn, '__self__'):
+        return
+    redis_store = getattr(fn.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
+        return
+    fn_name = fn.__qualname__
+    input_key = '{}:inputs'.format(fn_name)
+    output_key = '{}:outputs'.format(fn_name)
+    fn_call_count = 0
+
+    if redis_store.exists(fn_name) != 0:
+        fn_call_count = int(redis_store.get(fn_name))
+
+    print('{} was called {} times:'.format(fn_name, fn_call_count))
+    fn_inputs = redis_store.lrange(input_key, 0, -1)
+    fn_outputs = redis_store.lrange(output_key, 0, -1)
+
+    for fn_input, fn_output in zip(fn_inputs, fn_outputs):
+        print('{}(*{}) -> {}'.format(
+            fn_name,
+            fn_input.decode('utf-8'),
+            fn_output,
+        ))
+
+
 class Cache:
     '''Class for caching data in Redis'''
 
